532ef4683385093b63cce00eb2786040cc934ef9
veqis <alyssonhenrique000@gmail.com>
2025-02-05 07:48:43 -0300

    add tela vizualização detalhada desktop, caso pessoa consiga acessar

--- src/root/paginas/postagem_detalhes.js
+++ src/root/paginas/postagem_detalhes.js
@@ -6,6 +6,7 @@ import Editar from '../componentes/editar'
 import { RiArrowLeftSLine } from 'react-icons/ri'
 import { PiSirenDuotone } from 'react-icons/pi'
 import { Modal, Box, TextField, CircularProgress } from '@mui/material'
+import { isBrowser } from 'react-device-detect'
 
 function Detalhes() {
     const navigate = useNavigate()
@@ -23,6 +24,11 @@ function Detalhes() {
     const token = localStorage.getItem('token')
     const id_user = localStorage.getItem('id')
     const tipo = localStorage.getItem('tipo')
+    const statusInfo = {
+        1: { emoji: '🔴', texto: 'Pendente' },
+        2: { emoji: '🟢', texto: 'Resolvido' },
+        3: { emoji: '⚠️', texto: 'Falsa' },
+    }
 
     const fetchPostagem = async () => {
         try {
@@ -243,6 +249,180 @@ function Detalhes() {
     const nomePerfil = perfilData?.first_name || 'Anônimo'
     const isCurrentUser = parseInt(postagem?.usuario) === parseInt(id_user)
     const isTipoOuvidoria = tipo === "ouvidoria"
+    const { emoji, texto } = statusInfo[postagem?.status] || statusInfo[1]
+
+    if (isBrowser) {
+        return (
+            <div>
+                <div className='fixed top-0 start-0'>
+                    <NavBar />
+                </div>
+                <div className='flex mt-20 bg-white h-[60%] overflow-hidden '>
+                    <div className='w-[60%]'>
+                        <div className='flex justify-between bg-white rounded-t-lg p-3'>
+                            <div className='flex gap-2'>
+                                <img 
+                                    className='h-10 w-10 rounded-full object-cover' 
+                                    src={fotoPerfil} 
+                                    alt={`Perfil de ${nomePerfil}`} 
+                                />
+                                <div>
+                                    <h1 className='font-semibold text-xl -mb-1 truncate w-[70%]'>{nomePerfil}</h1>
+                                    <h1 className='font-[400] text-sm text-gray-400'>
+                                        Criado em: {data} às {hora}
+                                    </h1>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div className={`w-full h-full`}>
+                            <img 
+                                alt={`Imagem de ${nomePerfil}`} 
+                                src={postagem?.imagem ? `https://api.nero.lat/${postagem?.imagem}` : '/images/sem-imagem.png'}
+                                className='w-full h-full object-cover' 
+                            />
+                        </div>
+                    </div>
+                    <div className='flex flex-col w-full py-5 px-8'>
+
+                        <div className='flex justify-between items-center'>
+                            <div className='flex flex-col font-semibold '>
+                                <div className='flex  mb-1'>
+                                    <h1>{emoji}</h1>
+                                    <div className='flex flex-col'>
+                                        <h1 className='ml-1'>{texto}</h1>
+                                        <h1 className='text-sm font-normal ml-1 truncate'>{postagem?.titulo}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex'>{renderAlertas(postagem?.votos)}</div>
+                        </div>
+
+                        <div className='flex flex-col mt-3'>
+                            <h1 className='font-semibold'>Descricao</h1>
+                            <h1>{postagem?.descricao}</h1>
+                        </div>
+
+                        {comentarios.length > 0 && <h1 className='font-semibold mt-3'>Comentários</h1>}
+                        <div className='flex flex-col overflow-auto gap-2'>
+                            {comentarios.map((comentario) => (
+                                <div key={comentario.id} className='h-full w-full mb-3'>
+                                    <Comentario
+                                        texto={comentario.texto}
+                                        usuario={comentario.usuario}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                        {!isCurrentUser && (
+                            !isTipoOuvidoria ? (
+                                <div className='flex gap-2 mt-4 mb-[100px]'>
+                                    <button className='botao-estilo-1' onClick={() => setShowComentarioModal(true)}>
+                                        Comentar
+                                    </button>
+                                    <button className='botao-estilo-2' onClick={() => setShowAvaliacaoModal(true)}>
+                                        Avaliar
+                                    </button>
+                                </div>
+                            ) : (
+                                <div className='flex gap-2 mt-4 mb-[100px]'>
+                                    <button className='botao-estilo-1' onClick={() => setShowComentarioModal(true)}>
+                                        Comentar
+                                    </button>
+                                    <button disabled={loadingR} className='botao-estilo-1' onClick={() => { setLoadingF(true); alterarStatusReclama(3); }}>
+                                        {loadingF ? <CircularProgress sx={{ mb: -0.5 }} size={20} color="inherit" /> : 'Reportar'}
+                                    </button>
+                                    <button disabled={loadingF} className='botao-estilo-2' onClick={() => { setLoadingR(true); alterarStatusReclama(2); }}>
+                                        {loadingR ? <CircularProgress sx={{ mb: -0.5 }} size={20} color="inherit" /> : 'Resolver'}
+                                    </button>
+                                </div>
+                            )
+                        )}
+                        {isCurrentUser && (
+                            <div className='flex gap-2 mt-4 mb-[100px]'>
+                                <button className='botao-estilo-1' onClick={() => setShowComentarioModal(true)}>
+                                    Comentar
+                                </button>
+                                <button className='botao-estilo-1' onClick={() => setShowEditarModal(true)}>
+                                    Editar
+                                </button>
+                                <button className='botao-estilo-2' onClick={() => enviarFormulario()}>
+                                    Enviar Formulario
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                    <Modal open={showComentarioModal} onClose={() => setShowComentarioModal(false)}>
+                        <Box
+                            sx={{
+                                ...modalStyle,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                multiline
+                                rows={4}
+                                value={novoComentario}
+                                onChange={(e) => setNovoComentario(e.target.value)}
+                                placeholder="Escreva seu comentário aqui"
+                                margin="normal"
+                            />
+                            <div style={{ alignSelf: 'flex-end', marginTop: '16px' }}>
+                                <button className="botao-estilo-1 px-4" onClick={enviarComentario}>
+                                    Enviar
+                                </button>
+                            </div>
+                        </Box>
+                    </Modal>
+
+                    <Modal open={showAvaliacaoModal} onClose={() => setShowAvaliacaoModal(false)}>
+                        <Box
+                            sx={{
+                                ...modalStyle,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <div style={{ display: 'flex', justifyContent: 'center', justifyItems: 'center', marginBottom: '10px' }}>
+                                {[1, 2, 3, 4, 5].map((nivel) => (
+                                    <PiSirenDuotone
+                                        key={nivel}
+                                        size={40}
+                                        color={nivel <= avaliacao ? 'red' : 'gray'}
+                                        onClick={() => setAvaliacao(nivel)}
+                                        style={{ cursor: 'pointer', margin: '0 8px' }}
+                                    />
+                                ))}
+                            </div>
+                            <div style={{ alignSelf: 'flex', justifyContent: 'center', justifyItems: 'center' }}>
+                                <button className="botao-estilo-1 px-4" onClick={enviarAvaliacao}>
+                                    Enviar
+                                </button>
+                            </div>
+                        </Box>
+                    </Modal>
+
+                    {postagem && 
+                        <Editar
+                            id={postagem?.id}
+                            setShowEditarModal={setShowEditarModal}
+                            showEditarModal={showEditarModal}
+                            titulo={postagem?.titulo}
+                            descricao={postagem?.descricao}
+                            natureza={postagem?.natureza}
+                            funcUpdate={fetchPostagem}
+                        />
+                    }
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className='relative flex flex-col h-screen w-screen bg-[#e9e8e8] overflow-auto'>
